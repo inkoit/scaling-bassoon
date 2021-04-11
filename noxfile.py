3b95ee23e4d0ea0138bec3540de7bc7897c8b7f2
Nikita Subbotin <sub.nik.and@gmail.com>
2021-04-11 18:04:42 +0300

    Use black

--- noxfile.py
+++ noxfile.py
@@ -2,6 +2,7 @@ import nox
 
 
 locations = "src", "tests", "noxfile.py"
+nox.options.sessions = "lint", "tests"
 
 
 @nox.session(python=["3.9", "3.8"])
@@ -14,5 +15,12 @@ def tests(session):
 @nox.session(python=["3.9", "3.8"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8")
+    session.install("flake8", "flake8-black")
     session.run("flake8", *args)
+
+
+@nox.session(python="3.8")
+def black(session):
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
