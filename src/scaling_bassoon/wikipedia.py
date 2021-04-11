e0934cbe0f1b1540f7999f2725a43345fe7a24bb
Nikita Subbotin <sub.nik.and@gmail.com>
2021-04-11 18:01:09 +0300

    Use flake8

--- src/scaling_bassoon/wikipedia.py
+++ src/scaling_bassoon/wikipedia.py
@@ -2,13 +2,14 @@ import click
 import requests
 
 
+API_URL = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
+
+
 def random_page(language="en"):
-    api_url = f"https://{language.lower()}.wikipedia.org/api/rest_v1/page/random/summary"
     try:
-        with requests.get(api_url) as response:
+        with requests.get(API_URL.format(language=language)) as response:
             response.raise_for_status()
             return response.json()
     except requests.RequestException as error:
         message = str(error)
         raise click.ClickException(message)
-
