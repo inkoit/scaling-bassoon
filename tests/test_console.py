5491fcac2d62b6383836c9a5abfb3bff401e47cc
Nikita Subbotin <sub.nik.and@gmail.com>
2021-04-11 17:22:55 +0300

    Add nox, pytest and tests themselves

---
+++ tests/test_console.py
@@ -0,0 +1,35 @@
+import click.testing
+import pytest
+
+from scaling_bassoon import console
+
+
+@pytest.fixture
+def mock_requests_get(mocker):
+    mock = mocker.patch("requests.get")
+    mock.return_value.__enter__.return_value.json.return_value = {
+        "title": "Lorem Ipsum",
+        "extract": "Lorem ipsum dolor sit amet",
+    }
+    return mock
+
+
+@pytest.fixture
+def runner():
+    return click.testing.CliRunner()
+
+
+def test_main_succeeds(runner, mock_requests_get):
+    result = runner.invoke(console.main)
+    assert result.exit_code == 0
+
+
+def test_main_prints_title(runner, mock_requests_get):
+    result = runner.invoke(console.main)
+    assert "Lorem Ipsum" in result.output
+
+
+def test_main_invokes_requests_get(runner, mock_requests_get):
+    runner.invoke(console.main)
+    assert mock_requests_get.called
+
