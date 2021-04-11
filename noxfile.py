9d265b609c2bb691ec2aa12069a654e38f4c1bc0
Nikita Subbotin <sub.nik.and@gmail.com>
2021-04-11 18:27:03 +0300

    Use vulnerabilites check tools

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,9 @@
+import tempfile
+
 import nox
 
 locations = "src", "tests", "noxfile.py"
-nox.options.sessions = "lint", "tests"
+nox.options.sessions = "lint", "safety", "tests"
 
 
 @nox.session(python=["3.9", "3.8"])
@@ -14,7 +16,9 @@ def tests(session):
 @nox.session(python=["3.9", "3.8"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8", "flake8-black", "flake8-bugbear", "flake8-isort")
+    session.install(
+        "flake8", "flake8-bandit", "flake8-black", "flake8-bugbear", "flake8-isort"
+    )
     session.run("flake8", *args)
 
 
@@ -23,3 +27,19 @@ def black(session):
     args = session.posargs or locations
     session.install("black")
     session.run("black", *args)
+
+
+@nox.session(python="3.8")
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install("safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
